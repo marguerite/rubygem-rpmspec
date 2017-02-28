ffad9e183275655a2ef003ddbf25578a61406536
marguerite <i@marguerite.su>
2017-02-28 18:44:13 +0800

    start working on parser

--- lib/rpmspec/parser.rb
+++ lib/rpmspec/parser.rb
@@ -1,98 +1,73 @@
-require 'ostruct'
-
+require 'pp'
 module RPMSpec
   class Parser
-    def initialize(specfile)
-      raise RPMSpec::Exception, 'specfile not found.' unless File.exist?(specfile)
-      @file = open(specfile, 'r:UTF-8').read
+    def initialize(file)
+      raise RPMSpec::Exception, 'specfile not found.' unless File.exist?(file)
+      text = open(file, 'r:UTF-8').read
+      @subpackages = subpackages?(text) ? subpackages(text) : []
+      @text = subpackages?(text) ? strip_subpackages(@subpackages, text) : text
     end
 
-    def parse(_file = @file)
-      specobj = OpenStruct.new
+    def parse(text=@text)
+      p @text
     end
 
-    private
-
-    def legal_section(file)
-      file.match(/.*#\n/m)[0]
+    def subpackages?(text)
+      text.scan('%package').empty? ? false : true
     end
 
-    def name_section(file)
-      file.match(/Name:.*\n/)[0]
-    end
-
-    def version_section(file)
-      file.match(/Version:.*\n/)[0]
-    end
-
-    def release_section(file)
-      file.match(/Release:.*\n/)[0]
-    end
-
-    def summary_section(file)
-      file.match(/Summary:.*\n/)[0]
-    end
-
-    def license_section(file)
-      file.match(/License:.*\n/)[0]
-    end
-
-    def rpmgroup_section(file)
-      file.match(/Group:.*\n/)[0]
-    end
-
-    def homepage_section(file)
-      file.match(/Url:.*\n/)[0]
-    end
-
-    def buildroot_section(file)
-      file.match(/BuildRoot:.*\n/)[0]
-    end
-
-    def subpackages?(file = @file)
-      file.scan('%package').empty? ? false : true
-    end
-
-    def find_subpackages(file = @file)
-      file.scan(/%package.*?\n\n/m).map do |s|
-        s_name = s.match(/%package.*?\n/)[0].sub('%package', '').sub('-n', '').strip!
-        # use single line match to find "%description devel"
-        s_desc_start = file.match(/%description.*?#{s_name}/)[0]
-        s_desc = file.match(/#{s_desc_start}[^%]*\n\n/m)[0]
+    def subpackages(text)
+      text.scan(/%package.*?\n\n/m).map do |s|
+        s_name = s.match(/%package.*?\n/)[0].sub('%package','').sub('-n','').strip!
+        # use single line match to find '%description devel'
+        s_desc_start = text.match(/%description.*?#{s_name}/)[0]
+        s_desc = text.match(/#{s_desc_start}[^%]*\n\n/m)[0]
         # use single line match to find "%file devel"
-        s_file_start = file.match(/%file.*?#{s_name}/)[0]
-        s_file = file.match(/#{s_file_start}.*?\n\n/m)[0]
+        s_file_start = text.match(/%file.*?#{s_name}/)[0]
+        s_file = text.match(/#{s_file_start}.*?\n\n/m)[0]
         [s, s_desc, s_file]
       end
     end
 
-    def strip_subpackages(file = @file)
-      arr = find_subpackages
-      arr.each do |s|
-        s.each do |i|
-          file.sub!(i, '')
+    def strip_subpackages(subpackages,text)
+      subpackages.each {|s| s.each {|i| text.sub!(i, '') } }
+      text
+    end
+
+    # find texts contains specified tags and conditional tags
+    def find(tag,text)
+      # break specfile to lines
+      arr = text.split("\n")
+      tags = []
+      line_numbers = []
+      result = ''
+      # loop the lines
+      arr.each do |l|
+        # must start with the tag
+        next unless l.start_with?(tag)
+        index = arr.index(l)
+        # Find the previous and next line of the tag line
+        # to find the possible conditionals.
+        # Use line_numbers to hold the line number of the
+        # previous and next line, to avoid duplicate case
+        # since some line's next line may be other's
+        # previous line.
+        # The insert order to the tags array is important
+        # because we form text later using this order.
+        if index > 0 && !line_numbers.include?(index - 1) && arr[index - 1].start_with?('%')
+          line_numbers << index - 1
+          tags << arr[index - 1]
+        end
+        tags << l
+        if (index < arr.size - 1) && !line_numbers.include?(index + 1) && arr[index + 1].start_with?('%')
+          line_numbers << index + 1
+          tags << arr[index + 1]
         end
       end
-      file
-    end
-
-    def macros_section(file = @file)
-      file.scan(/%define.*?\n/)
-    end
-
-    def find_macros(_file = @file)
-      macros = {}
-      macros_section.each do |s|
-        arr = []
-        s = s.sub('%define', '').strip!
-        arr = if s.index("\t")
-                s.split(/\t+/)
-              else
-                s.split(/\s+/)
-              end
-        macros[arr[0]] = arr[1]
-      end
-      macros
+      tags.each {|l| result << l + "\n" }
+      result
     end
   end
 end
+require './exception.rb'
+pp RPMSpec::Parser.new('kcm5-fcitx.spec').parse
