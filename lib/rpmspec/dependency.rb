9ce6625e261e6f62c603875a8ac0631c09d4d406
Marguerite Su <i@marguerite.su>
2017-08-14 06:17:30 +0000

    [refactor]break parse_tag() to two functions

--- lib/rpmspec/dependency.rb
+++ lib/rpmspec/dependency.rb
@@ -9,43 +9,38 @@ module RPMSpec
     end
 
     def parse(tag)
-      parse_tag(tag)
+      return unless tag?(tag)
+      s = @text.dup
+      # the conditional tags
+      conditionals = s.scan(/%if.*?%endif/m)
+      # strip conditional tags' contents
+      conditionals.each { |i| s.sub!(i, '') } unless conditionals.empty?
+      tag_struct = Struct.new(:name, :version, :conditionals, :modifier)
+      normals = parse_normal_tag(tag, s, tag_struct)
+      return normals if conditionals.empty?
+      conds = parse_conditional_tag(tag, conditionals, tag_struct)
+      normals + conds
     end
 
     def inspect(arr, tag)
       str = ''
       arr.each do |s|
-        if s.conditionals.nil?
-          str << if s.modifier.nil?
-                   "#{tag}:\s\s#{s.name}\n"
-                 else
-                   "#{tag}(#{s.modifier}):\s\s#{s.name}\n"
-                 end
-        else
-          s.conditionals.each { |i| str << "%if\s" + i + "\n" }
-          str << if s.modifier.nil?
-                   "#{tag}:\s\s#{s.name}\n"
-                 else
-                   "#{tag}(#{s.modifier}):\s\s#{s.name}\n"
-                 end
-          s.conditionals.size.times { str << "%endif\n" }
-        end
+        s.conditionals.each { |i| str << "%if\s" + i + "\n" } unless s.conditionals.nil?
+        str << if s.modifier.nil?
+                 "#{tag}:\s\s#{s.name}\n"
+               else
+                 "#{tag}(#{s.modifier}):\s\s#{s.name}\n"
+               end
+        s.conditionals.size.times { str << "%endif\n" } unless s.conditionals.nil?
       end
       str
     end
 
     private
 
-    def parse_tag(tag)
-      return unless tag?(tag)
-      s = @text.dup
-      # the conditional tags
-      conditionals = s.scan(/%if.*?%endif/m)
-      # strip condtional tags' contents
-      conditionals.each { |i| s.sub!(i, '') } unless conditionals.empty?
-      tag_struct = Struct.new(:name, :version, :conditionals, :modifier)
-      normals = []
-      s.split("\n").reject(&:empty?).each do |i|
+    def parse_normal_tag(tag, text, struct)
+      arr = []
+      text.split("\n").reject(&:empty?).each do |i|
         # modifier: modifier to the tag, eg Requires(post)
         modifier = Regexp.last_match(1) if i =~ /\((.*)\):/
         content = i.sub!(/#{tag}.*?:/, '').strip!
@@ -53,18 +48,21 @@ module RPMSpec
           # have an expression Requires: abc > 1.0.0
           version = Regexp.last_match(0)
           name = content.sub!(version, '').strip!
-          normals << tag_struct.new(name, version, nil, modifier)
+          arr << struct.new(name, version, nil, modifier)
         elsif content =~ /\s+/
           # multiple entries in one line: Requires: example example1
           content.split(/\s+/).each do |j|
-            normals << tag_struct.new(j, nil, nil, modifier)
+            arr << struct.new(j, nil, nil, modifier)
           end
         else
-          normals << tag_struct.new(content, nil, nil, modifier)
+          arr << struct.new(content, nil, nil, modifier)
         end
       end
-      return normals if conditionals.empty?
-      conds = []
+      arr
+    end
+
+    def parse_conditional_tag(tag, conditionals, struct)
+      arr = []
       conditionals.each do |i|
         RPMSpec::Conditional.new(i).parse.each do |j|
           modifier = Regexp.last_match(1) if j.name =~ /\((.*)\):/
@@ -72,17 +70,17 @@ module RPMSpec
           if content =~ /(>|=|<).*$/
             version = Regexp.last_match(0)
             name = content.sub!(version, '').strip!
-            conds << tag_struct.new(name, version, j.conditionals, modifier)
+            arr << struct.new(name, version, j.conditionals, modifier)
           elsif content =~ /\s+/
             content.split(/\s+/).each do |k|
-              conds << tag_struct.new(k, nil, j.conditionals, modifier)
+              arr << struct.new(k, nil, j.conditionals, modifier)
             end
           else
-            conds << tag_struct.new(content, nil, j.conditionals, modifier)
+            arr << struct.new(content, nil, j.conditionals, modifier)
           end
         end
       end
-      normals + conds
+      arr
     end
   end
 end
