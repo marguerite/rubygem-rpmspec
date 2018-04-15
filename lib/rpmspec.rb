1cea36b54fd0b6220e936a111ad1fcafca6686f6
marguerite <i@marguerite.su>
2018-04-15 15:03:02 +0800

    return text, array or openstruct as result

--- lib/rpmspec.rb
+++ lib/rpmspec.rb
@@ -4,3 +4,18 @@ Dir.glob(path + '/*').each do |i|
   require dir + '/' + File.basename(i) if File.basename(i) =~ /\.rb/
 end
 require 'ostruct'
+
+def form_result(**args)
+  hash = {}
+  method(__method__).parameters.map do |_, name|
+    hash = binding.local_variable_get(name) if name == :args
+  end
+  # reject nil values
+  hash = hash.reject { |_k, v| v.nil? }
+
+  return hash.values[0] if hash.values.size == 1
+
+  OpenStruct.new(hash.each_with_object({}) do |(k, v), m|
+    m[k] = v
+  end)
+end
--- lib/rpmspec/subpackage.rb
+++ lib/rpmspec/subpackage.rb
@@ -10,28 +10,27 @@ module RPMSpec
       @text.to_enum(:scan, /^%package\s+(-n\s+)?([a-zA-Z0-9%{}\-_]+)\n/m)
            .map { Regexp.last_match }
            .map! do |i|
-             pkg = OpenStruct.new
-
              name = RPMSpec::Tag.new(@text, @args).send(:replace_macro, i[2])
-             pkg.name = if i[1]
-                          name
-                        else
-                          @args[:name] + '-' + name
-                        end
-             pkg.desc = find_description(@text, i[2])[3]
+             pkgname = if i[1]
+                         name
+                       else
+                         @args[:name] + '-' + name
+                       end
+             desc = find_description(@text, i[2])[3]
 
              tag_text = find_tag_text(@text, i[0])
+             tag_hash = {}
              TAGS.each do |j|
                tag = RPMSpec::Tag.new(tag_text, @args).send(j.downcase.to_sym)
-               pkg[j.downcase] = tag unless tag.nil?
+               tag_hash[j.downcase.to_sym] = tag unless tag.nil?
              end
 
-             pkg.files = find_files(@text, i[2])
+             files = find_files(@text, i[2])
              scripts = find_scripts(@text, i[2])
              conditional = RPMSpec::Conditional.new(@text, i[0]).parse
-             pkg.scripts = scripts unless scripts.nil?
-             pkg.conditional = conditional unless conditional.nil?
-             pkg
+
+             RPMSpec.send(:form_result, name: pkgname, description: desc, files: files,
+                                        scripts: scripts, conditional: conditional, **tag_hash)
            end
     end
 
@@ -43,19 +42,11 @@ module RPMSpec
            .each do |i|
              desc = find_description(@text, i[2])
              tag_text = find_tag_text(@text, i[0])
-             files = find_files(@text, i[2], true)
+             [i[0], desc[0], tag_text].each { |j| text.sub!(j, '') }
+             text = strip_files(find_files(@text, i[2], true), i[2], text)
              scripts = find_scripts(@text, i[2])
-
-             text.sub!(i[0], '')
-             text.sub!(desc[0], '')
-             text.sub!(tag_text, '')
-
-             # or defattr for the main file block will be stripped too.
-             text.gsub!(/^%files\s+(-n\s+)?#{Regexp.escape(i[2])}\n(%defattr.*?\n)?/, '')
-             files.files[1..-1].each { |t| text.gsub!(/^#{Regexp.escape(t.file)}/, '') if text.index(t.file) }
-
              next if scripts.nil?
-             scripts.each { |s| text.gsub!(s.text, '') }
+             text = strip_scripts(scripts, text)
            end
       # strip the useless self-closed conditional blocks
       text.gsub(/^%if.*?\n%endif.*?\n/m, '')
@@ -79,11 +70,8 @@ module RPMSpec
       m = text.match(/^%files(\s+)?(-n\s+)?#{confident_name(name)}(-f.*?)?\n(((?!%files)(?!%changelog).)*)\n(\s+)?\n/m)
       cond_text = @text.match(/^%if((?!%files).)*?#{Regexp.escape(m[0])}/m)
       conditional = RPMSpec::Conditional.new(cond_text[0], m[0].strip!.gsub!(/%endif\Z/m, '')).parse unless cond_text.nil?
-      s = OpenStruct.new
-      s.files = parse_file(m[4], raw)
-      s.list = m[3].sub!(/-f\s+/, '') unless m[3].nil?
-      s.conditional = conditional unless conditional.nil?
-      s
+      list = m[3].sub!(/-f\s+/, '') unless m[3].nil?
+      RPMSpec.send(:form_result, files: parse_file(m[4], raw), list: list, conditional: conditional)
     end
 
     def parse_file(files, raw)
@@ -92,18 +80,33 @@ module RPMSpec
         if i =~ /^%.*if/ || i.empty?
           nil
         else
-          cond = RPMSpec::Conditional.new(files, i).parse
           file = if raw
                    i
                  else
                    RPMSpec::Tag.new(@text, @args).send(:replace_macro, i)
                  end
-          s = OpenStruct.new
-          s.file = file
-          s.conditional = cond unless cond.nil?
-          s
+          RPMSpec.send(:form_result, file: file,
+                                     conditional: RPMSpec::Conditional.new(files, i).parse)
         end
       end.compact
+    end
+
+    def strip_files(files, name, text)
+      name = confident_name(name)
+      # or defattrs will be all stripped
+      text.gsub!(/^%files\s+(-n\s+)?#{name}\n(%defattr.*?\n)?/, '')
+      files = if files.instance_of?(Array)
+                files[1..-1]
+              elsif files.instance_of?(OpenStruct)
+                files.files[1..-1]
+              else
+                raise 'Unhandled type of files' + files.inspect
+              end
+      files.each do |t|
+        t = t.file if t.instance_of?(OpenStruct)
+        text.gsub!(/^#{Regexp.escape(t)}/, '') if text.index(t)
+      end
+      text
     end
 
     def find_scripts(text, name)
@@ -111,12 +114,22 @@ module RPMSpec
               .map { Regexp.last_match }
       return if m.empty?
       m.map! do |i|
-        s = OpenStruct.new
-        conditional = RPMSpec::Conditional.new(@text, i[0]).parse
-        s.text = i[0]
-        s.conditional = conditional unless conditional.nil?
-        s
+        RPMSpec.send(:form_result, script: i[0],
+                                   conditional: RPMSpec::Conditional.new(@text, i[0]).parse)
       end
+    end
+
+    def strip_scripts(scripts, text)
+      scripts.each do |i|
+        if i.instance_of?(String)
+          text.gsub!(i, '')
+        elsif i.instance_of?(OpenStruct)
+          text.gsub!(i.script, '')
+        else
+          raise 'Unhandled type of scripts' + scripts.inspect
+        end
+      end
+      text
     end
   end
 end
